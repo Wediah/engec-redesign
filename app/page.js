42e08ef6e240ee29810b164a163636988bdf1cd9
Wediah <100675006+Wediah@users.noreply.github.com>
2024-02-28 02:58:25 +0000

    Update background image and add heading and subheading to Home component

--- app/page.js
+++ app/page.js
@@ -3,7 +3,13 @@ import Background from "/public/Aburi1.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen px-4" style={{ backgroundImage: 'url(Background)'}}>
+    <main className="flex px-4 bg-no-repeat bg-cover bg-center bg-black m-6" style={{ backgroundImage: 'url(Aburi1.jpg)'}}>
+
+    <div className='text-left pt-48 h-screen'>
+        <h1 className=' text-7xl lg:text-9xl font-bold text-white '>ENGEC GROUP</h1>
+        <h4 className='flex text-xl text-gray-100 pb-2 font-medium'>WE BUILD STORIES, NOT JUST STRUCTURES.</h4>
+        
+    </div>
       
     </main>
   );
