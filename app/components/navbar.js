e5a3ac0024e1a130d4847806f77623fa19434e6a
Wediah <100675006+Wediah@users.noreply.github.com>
2024-03-03 23:52:03 +0000

    Change link text to uppercase in navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -135,7 +135,7 @@ function Navbar() {
                                 href="/projects"
                                 className="cursor-pointer hover:text-yellow-300 text-black block px-3 py-2 rounded-md  font-bold text-left dark:text-white text-6xl"
                             >
-                                Projects
+                                PROJECTS
                             </Link>
                             <Link
                             onClick={() => setIsOpen(!isOpen)}
