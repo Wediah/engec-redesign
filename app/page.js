dd3bcd512970b17f34fcf12c250c3cb2f317564d
Wediah <100675006+Wediah@users.noreply.github.com>
2024-03-05 00:21:29 +0000

    Refactor page layout and update content

--- app/page.js
+++ app/page.js
@@ -3,14 +3,26 @@ import Background from "/public/Aburi1.jpg";
 
 export default function Home() {
   return (
-    <div className="flex px-4 ">
+    <main className="  mx-auto ">
 
-    <div className='text-left pb-56 w-screen bg-no-repeat pl-4 bg-cover bg-center bg-black m-6' style={{ backgroundImage: 'url(Aburi1.jpg)'}}>
-        <h1 className=' text-7xl lg:text-9xl font-bold text-white '>ENGEC GROUP</h1>
-        <h4 className='flex text-xl text-gray-100 pb-2 font-medium'>WE BUILD STORIES, NOT JUST STRUCTURES.</h4>  
+    <div className='text-center pt-36 pb-56 bg-no-repeat pl-4 bg-cover bg-center bg-black px-4 m-6' style={{ backgroundImage: 'url(Aburi1.jpg)'}}>
+        <h1 className='text-7xl lg:text-9xl font-black text-white'>ENGEC GROUP</h1>
+        <h4 className='text-xl text-gray-100 pb-2 font-bold'>WE BUILD STORIES, NOT JUST STRUCTURES.</h4>  
     </div>
 
-      
+    <div className="bg-white text-black pt-3">
+      <div className=" px-4 m-6">
+      <div className="justify-between flex">
+        <p className="text-gray-500">OUR APPROACH</p>
+        <p className="font-bold">WITH RESEARCH-DRIVEN</p>
+      </div>
+      <p className="text-black font-bold">
+      APPROACHES, WE FACTOR IN ALL YOUR NEEDS AND PRESENT YOU AN EXPERIENCE THAT FITS YOUR IMAGINATION WHETHER IT BEING A UNIQUE LANSCAPE, A BUSTLING CITY SCENARIO OR THE LIVELY PEOPLE AND THEIR MOVEMENTS. 
+      </p>
+      </div>
     </div>
+
+
+    </main>
   );
 }
