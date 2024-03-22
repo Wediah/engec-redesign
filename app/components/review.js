83733ca5281dec34612af9dbc290de101319f9a9
Wediah <100675006+Wediah@users.noreply.github.com>
2024-03-22 04:18:41 +0000

    Refactor review component layout

--- app/components/review.js
+++ app/components/review.js
@@ -5,16 +5,16 @@ async function Review() {
     const reviews = await Reviews();
 
   return (
-    <div className="bg-white">
-      <div className="px-4">
+    <div className="bg-white px-4 m-6">
+      <div className="">
       <h1 className="text-black text-center font-bold text-xl md:text-4xl">WHAT OUR CLIENTS SAY ABOUT US</h1>
       </div>
 
       <div className="text-black">
-            <div className='bg-white'>
+            <div className='bg-white flex '>
               { reviews && reviews.map((review, index) => {
                     return (
-                        <div className="w-3/5 h-3/5 gap-2" key={index}>
+                        <div className="w-3/5 h-3/5 gap-2 " key={index}>
                         <p className="text-md font-bold text-black">{review.description}</p>
                         <h1 className="text-md font-bold text-black">{review.name}</h1>
                         </div>
